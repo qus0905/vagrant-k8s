ac99576053280d707503a0c623bf3ad2bb95d8de
jybyun <jybyun@osci.kr>
2024-10-16 13:29:29 +0900

    modified

--- k8s_env_build.sh
+++ k8s_env_build.sh
@@ -15,9 +15,6 @@ sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
 echo "192.168.1.10 control-k8s" >> /etc/hosts
 for (( i=1; i<=$1; i++  )); do echo "192.168.1.10$i worker$i-k8s" >> /etc/hosts; done
 
-# config DNS  
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-
 # sshd config setting 
 sed -i -e '15s:^#::' /etc/ssh/sshd_config
 sed -i -e '39s:^#::' /etc/ssh/sshd_config
